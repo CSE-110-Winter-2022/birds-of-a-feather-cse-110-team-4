a16a9496835b75c875fa699290ad9044b78c95b6
h1zhao <h1zhao@ucsd.edu>
2022-02-02 22:52:46 -0800

    finish build the frame of database

---
+++ app/src/main/java/com/example/birdsoffeather/model/IPerson.java
@@ -0,0 +1,9 @@
+package com.example.birdsoffeather.model;
+
+import java.util.List;
+
+public interface IPerson {
+    public String getName();
+    public String getURL();
+    public List<String> getCourses();
+}
---
+++ app/src/main/java/com/example/birdsoffeather/model/db/Person.java
@@ -0,0 +1,20 @@
+package com.example.birdsoffeather.model.db;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+
+@Entity(tableName = "persons")
+public class Person {
+
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    public int personId;
+
+    @ColumnInfo(name = "name")
+    public String name;
+
+    @ColumnInfo(name = "headshot")
+    public String imageURL;
+}
