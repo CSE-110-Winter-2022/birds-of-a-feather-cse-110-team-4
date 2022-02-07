89ac0cbd966f77c272cf82ee11afd3211f0168c1
YilangHe <yih022@ucsd.edu>
2022-02-07 02:00:58 -0800

    Implement 2 test cases for AddClassActivity

--- app/src/test/java/com/example/birdsoffeather/AddClassActivityTest.java
+++ app/src/test/java/com/example/birdsoffeather/AddClassActivityTest.java
@@ -2,25 +2,46 @@ package com.example.birdsoffeather;
 
 import static org.junit.Assert.assertEquals;
 
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.example.birdsoffeather.model.db.AppDatabase;
+import com.example.birdsoffeather.model.db.CoursesDao;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class AddClassActivityTest {
+
     @Rule
     public ActivityScenarioRule rule =
             new ActivityScenarioRule<AddClassActivity>(AddClassActivity.class);
 
     @Test
-    public void test1() {
+    public void testPageTextUI() {
         ActivityScenario scenario = rule.getScenario();
         scenario.onActivity(activity -> {
-            assertEquals(2, 2);
+            TextView title = (TextView)  activity.findViewById(R.id.textView2);
+            assertEquals("Add Classes", title.getText());
+        });
+    }
+
+    @Test
+    public void testInitialState() {
+        ActivityScenario scenario = rule.getScenario();
+        scenario.onActivity(activity -> {
+            EditText editSubject = (EditText)  activity.findViewById(R.id.editSubject);
+            assertEquals("", editSubject.getText().toString());
+            EditText editCourse = (EditText)  activity.findViewById(R.id.editCourse);
+            assertEquals("", editCourse.getText().toString());
         });
     }
 }
