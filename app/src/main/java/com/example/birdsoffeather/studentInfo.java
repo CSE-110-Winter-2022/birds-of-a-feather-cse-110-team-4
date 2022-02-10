27082d61a791cb83f5c0bfb8720c3a6d8e55bd25
Stephan <spnguyen@ucsd.edu>
2022-02-10 15:11:48 -0800

    update date to the list

--- app/src/main/java/com/example/birdsoffeather/studentInfo.java
+++ app/src/main/java/com/example/birdsoffeather/studentInfo.java
@@ -6,24 +6,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import com.example.birdsoffeather.model.IPerson;
+import com.example.birdsoffeather.model.db.AppDatabase;
+
+import java.util.List;
 
 public class studentInfo extends AppCompatActivity {
-
+    private AppDatabase db;
+    private IPerson person;
+    private int personId = 0;
+    private  String name;
+    private List course;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_info);
 
-        TextView personNameView = findViewById(R.id.studentName);
+        db = AppDatabase.singleton(this);
+        person = db.personsWithCoursesDao().get(personId);
+        name = person.getName();
+        course = person.getCourses();
+        TextView nameView = findViewById(R.id.studentName);
+        nameView.setText(name);//Set name for user
+        TextView courseView = findViewById(R.id.studentName);
+        courseView.setText(course.toString());
 
-        Intent intent =getIntent();
 
-        String personName = intent.getStringExtra("Person_Name");
-        String [] personNotes = intent.getStringArrayExtra("Class_list");
 
-        setTitle(personName);
-        personNameView.setText(String.join("\n", personName));
+
     }
 
     public void backToList(View view) {
