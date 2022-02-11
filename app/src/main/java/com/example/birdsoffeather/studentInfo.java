21fb296442f5e4747706a1e34e629f2be4ba4832
Stephan <spnguyen@ucsd.edu>
2022-02-10 18:44:47 -0800

    update date to the list again with imageURL code

--- app/src/main/java/com/example/birdsoffeather/studentInfo.java
+++ app/src/main/java/com/example/birdsoffeather/studentInfo.java
@@ -3,12 +3,19 @@ package com.example.birdsoffeather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.example.birdsoffeather.model.IPerson;
 import com.example.birdsoffeather.model.db.AppDatabase;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
 public class studentInfo extends AppCompatActivity {
@@ -17,24 +24,46 @@ public class studentInfo extends AppCompatActivity {
     private int personId = 0;
     private  String name;
     private List course;
+    private String imageURL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_info);
 
+        //Getting data from data base (db)
         db = AppDatabase.singleton(this);
         person = db.personsWithCoursesDao().get(personId);
         name = person.getName();
         course = person.getCourses();
+        imageURL = person.getURL();
         TextView nameView = findViewById(R.id.studentName);
         nameView.setText(name);//Set name for user
         TextView courseView = findViewById(R.id.studentName);
         courseView.setText(course.toString());
 
+        ImageView i = (ImageView) findViewById(R.id.profile_picture_view);
 
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ImageView i = (ImageView)findViewById(R.id.profile_picture_view);
+                    Bitmap bitmap = BitmapFactory.decodeStream((InputStream)new URL(imageURL).getContent());
+                    runOnUiThread(new Runnable() {
 
+                        @Override
+                        public void run() {
 
+                            i.setImageBitmap(bitmap);
+
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     public void backToList(View view) {
