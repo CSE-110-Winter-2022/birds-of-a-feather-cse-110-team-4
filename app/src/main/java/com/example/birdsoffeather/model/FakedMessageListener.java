1de40ae1444357c45e3a35cef2ab9f57bc1dbdc4
XiangLi <jacklix0105@gmail.com>
2022-02-11 18:25:30 -0800

    isolated reader

--- app/src/main/java/com/example/birdsoffeather/model/FakedMessageListener.java
+++ app/src/main/java/com/example/birdsoffeather/model/FakedMessageListener.java
@@ -1,2 +1,24 @@
-package com.example.birdsoffeather.model;public class FakedMessageListener {
+package com.example.birdsoffeather.model;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
+
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class FakedMessageListener extends MessageListener{
+    private final MessageListener messageListener;
+    private final ScheduledExecutorService executor;
+
+    public FakedMessageListener(MessageListener realMessageListener, int frequency, String messageStr) {
+        this.messageListener = realMessageListener;
+        this.executor = Executors.newSingleThreadScheduledExecutor();
+
+        executor.scheduleAtFixedRate(()-> {
+            Message message = new Message(messageStr.getBytes(StandardCharsets.UTF_8));
+            this.messageListener.onFound(message);
+            this.messageListener.onLost(message);
+        }, 0, frequency, TimeUnit.SECONDS);
+    }
 }
