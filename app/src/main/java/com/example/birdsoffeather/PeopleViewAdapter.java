84bdb84b913bae3f7151c394adda11b04ece26c8
ademirkan <ademirkan@ucsd.edu>
2022-02-12 12:57:03 -0800

    Attempt at adding image + num to recyclerview, not tested

---
+++ app/src/main/java/com/example/birdsoffeather/PeopleViewAdapter.java
@@ -0,0 +1,93 @@
+package com.example.birdsoffeather;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.birdsoffeather.model.IPerson;
+import com.example.birdsoffeather.model.db.PersonWithCourses;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
+public class PeopleViewAdapter extends RecyclerView.Adapter<PeopleViewAdapter.ViewHolder> {
+//    private final List<? extends IPerson> people;
+    private final IPerson[] people;
+
+//    public PeopleViewAdapter(List<PersonWithCourses> people) {
+//        super();
+//        this.people = people;
+//    }
+
+    public PeopleViewAdapter(IPerson[] people) {
+        super();
+        this.people = people;
+    }
+
+    @NonNull
+    @Override
+    public PeopleViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view =
+                LayoutInflater.from(parent.getContext()).inflate(R.layout.people_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PeopleViewAdapter.ViewHolder holder, int position) {
+        holder.setPerson(people[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.people.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+
+        private final TextView personNameView;
+        private final ImageView personAvatarView;
+        private final TextView personNumClasses;
+        private IPerson person;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            this.personNameView = itemView.findViewById(R.id.person_row_name);
+            this.personAvatarView = itemView.findViewById(R.id.person_row_image);
+            this.personNumClasses = itemView.findViewById(R.id.person_row_num);
+
+            itemView.setOnClickListener(this);
+        }
+
+        public void setPerson(IPerson person) {
+            this.person = person;
+            this.personNameView.setText(person.getName());
+            this.personNumClasses.setText(person.getCourses().size());  // all classes, not in common for now. Fix later
+            try {
+                this.personAvatarView.setImageBitmap(BitmapFactory.decodeStream((InputStream)new URL(person.getURL()).getContent()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            Context context = view.getContext();
+            Intent intent = new Intent(context, PersonDetailActivity.class);
+            intent.putExtra("person_id", this.person.getId());
+            context.startActivity(intent);
+        }
+    }
+}
--- app/src/main/java/com/example/birdsoffeather/searchingActivity.java
+++ app/src/main/java/com/example/birdsoffeather/searchingActivity.java
@@ -1,20 +1,50 @@
 package com.example.birdsoffeather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.birdsoffeather.model.IPerson;
+import com.example.birdsoffeather.model.db.Person;
+import com.example.birdsoffeather.model.db.PersonWithCourses;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class searchingActivity extends AppCompatActivity {
+    protected RecyclerView personRecyclerView;
+    protected RecyclerView.LayoutManager personLayoutManager;
+    protected PeopleViewAdapter peopleViewAdapter;
+
+    protected IPerson[] testData = {
+            new PersonWithCourses(new Person(101, "Charles", "123"), new ArrayList<String>(Arrays.asList("CSE 111", "CSE 110"))),
+            new PersonWithCourses(new Person(102, "Stephan", "123"), new ArrayList<String>(Arrays.asList("CSE 111", "CSE 110"))),
+    };
+
+    //TODO: List to store fetch students
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //TODO: create database
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searching);
+        setTitle("Searching");
+
+        RecyclerView searchRecyclerView = findViewById(R.id.search_recycler_view);
+        personLayoutManager = new LinearLayoutManager(this);
+        searchRecyclerView.setLayoutManager(personLayoutManager);
+
+
     }
 
     public void searchonClick(View view) {
+        RecyclerView searchRecyclerView = findViewById(R.id.search_recycler_view);
+        peopleViewAdapter = new PeopleViewAdapter(testData);
+        searchRecyclerView.setAdapter(peopleViewAdapter);
     }
 
     public void backonClick(View view) {
