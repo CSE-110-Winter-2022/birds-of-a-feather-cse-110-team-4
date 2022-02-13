7c8a92da279ba85ae135a2f4831c9805be66fc73
ademirkan <ademirkan@ucsd.edu>
2022-02-13 12:58:40 -0800

    Changed unit tests because of bug

--- app/src/test/java/com/example/birdsoffeather/AddClassActivityTest.java
+++ app/src/test/java/com/example/birdsoffeather/AddClassActivityTest.java
@@ -2,25 +2,47 @@ package com.example.birdsoffeather;
 
 import static org.junit.Assert.assertEquals;
 
+import android.content.Context;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.lifecycle.Lifecycle;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.birdsoffeather.model.db.AppDatabase;
 import com.example.birdsoffeather.model.db.CoursesDao;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.IOException;
+
 @RunWith(AndroidJUnit4.class)
 public class AddClassActivityTest {
+    private AppDatabase db;
+    @Before
+    public void createDb() {
+        Context context = ApplicationProvider.getApplicationContext();
+//        db = Room.inMemoryDatabaseBuilder(context, AppDatabase.class).build();
+//        db = AppDatabase.singleton(context);
+//        db.useTestSingleton(context);
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase.class)
+                .allowMainThreadQueries()
+                .build();
+    }
+
+    @After
+    public void closeDb() throws IOException {
+        db.close();
+    }
 
     @Rule
     public ActivityScenarioRule rule =
@@ -37,6 +59,8 @@ public class AddClassActivityTest {
         });
     }
 
+    }
+
 //    @Test
 //    public void testInitialState() {
 //        ActivityScenario scenario = rule.getScenario();
@@ -47,4 +71,4 @@ public class AddClassActivityTest {
 //            assertEquals("", editCourse.getText().toString());
 //        });
 //    }
-}
+
