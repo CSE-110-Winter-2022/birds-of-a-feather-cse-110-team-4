3fc2a870fb381f500c387778026ffa3e48512d3c
Stephan <spnguyen@ucsd.edu>
2022-02-13 15:55:51 -0800

    working on test code

--- app/src/test/java/com/example/birdsoffeather/DataBaseTest.java
+++ app/src/test/java/com/example/birdsoffeather/DataBaseTest.java
@@ -8,6 +8,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.birdsoffeather.model.db.AppDatabase;
 import com.example.birdsoffeather.model.db.CoursesDao;
+import com.example.birdsoffeather.model.db.Person;
 import com.example.birdsoffeather.model.db.PersonWithCoursesDao;
 
 import org.junit.After;
@@ -23,16 +24,13 @@ import java.io.IOException;
 @RunWith(AndroidJUnit4.class)
 public class DataBaseTest {
     private PersonWithCoursesDao personWithCoursesDao;
-    private CoursesDao coursesDao;
     private AppDatabase db;
 
     @Before
     public void createDb() {
         Context context = ApplicationProvider.getApplicationContext();
-
-        db = AppDatabase.singleton(context);
+        db = AppDatabase.useTestSingleton(context);
         personWithCoursesDao = db.personsWithCoursesDao();
-        coursesDao = db.coursesDao();
     }
 
     @After
@@ -41,8 +39,11 @@ public class DataBaseTest {
     }
 
     @Test
-    public void testInsertPersonAndNote() throws Exception {
-
+    public void testInsertPerson() throws Exception {
+        Person testPerson1 = new Person(0,"Test Name 1","https://lh3.googleusercontent.com/pw/AM-JKLXQ2ix4dg-PzLrPOSMOOy6M3PSUrijov9jCLXs4IGSTwN73B4kr-F6Nti_4KsiUU8LzDSGPSWNKnFdKIPqCQ2dFTRbARsW76pevHPBzc51nceZDZrMPmDfAYyI4XNOnPrZarGlLLUZW9wal6j-z9uA6WQ=w854-h924-no?authuser=0");
+        Person testPerson2 = new Person(1,"Test Name 2","https://lh3.googleusercontent.com/pw/AM-JKLXQ2ix4dg-PzLrPOSMOOy6M3PSUrijov9jCLXs4IGSTwN73B4kr-F6Nti_4KsiUU8LzDSGPSWNKnFdKIPqCQ2dFTRbARsW76pevHPBzc51nceZDZrMPmDfAYyI4XNOnPrZarGlLLUZW9wal6j-z9uA6WQ=w854-h924-no?authuser=0");
+        db.personsWithCoursesDao().insert(testPerson1);
+        db.personsWithCoursesDao().insert(testPerson2);
         int numOfPerson = db.personsWithCoursesDao().count();
         System.out.println(numOfPerson);
         Assert.assertEquals(2, numOfPerson);
