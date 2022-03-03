58917746ba1fb53252fec7edb291bdc3a35eecbc
Noodles <yih022@ucsd.edu>
2022-03-02 19:52:24 -0800

    save session dialogue

--- app/src/main/java/com/example/birdsoffeather/searchingActivity.java
+++ app/src/main/java/com/example/birdsoffeather/searchingActivity.java
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.birdsoffeather.model.db.AppDatabase;
@@ -29,6 +34,8 @@ public class searchingActivity extends AppCompatActivity {
     protected RecyclerView personRecyclerView;
     protected RecyclerView.LayoutManager personLayoutManager;
     protected PeopleViewAdapter peopleViewAdapter;
+    final Context context = this;
+    private EditText userSessionInput;
 
     protected boolean btnStatus = true;
 
@@ -85,6 +92,48 @@ public class searchingActivity extends AppCompatActivity {
             btnStatus = !btnStatus;
             btn.setText("Start");
             //pop up window & save session
+
+            // get prompts.xml view
+            LayoutInflater li = LayoutInflater.from(context);
+            View promptsView = li.inflate(R.layout.addsession_dialog, null);
+
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                    context);
+
+            // set prompts.xml to alertdialog builder
+            alertDialogBuilder.setView(promptsView);
+
+            final EditText userInput = (EditText) promptsView
+                    .findViewById(R.id.dialogUserInput);
+
+            // set dialog message
+            alertDialogBuilder
+                    .setCancelable(false)
+                    .setPositiveButton("Save",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    // get user input and set it to result
+                                    // edit text
+//                                    userSessionInput.setText(userInput.getText());
+
+                                    //for the test
+                                    dialog.cancel();
+                                }
+                            })
+                    .setNegativeButton("Cancel",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,int id) {
+                                    dialog.cancel();
+                                }
+                            });
+
+            // create alert dialog
+            AlertDialog alertDialog = alertDialogBuilder.create();
+
+            // show it
+            alertDialog.show();
+
+
         }
 
     }
